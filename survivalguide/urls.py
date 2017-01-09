eac3fd9625f82482d58ffbbef097144c53295971
vagrant <vagrant@precise64.(none)>
2017-01-09 19:15:55 +0000

    first commit

---
+++ survivalguide/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+from views import HomePageView, SignUpView, LoginView
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'survivalguide.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'accounts/register/$', SignUpView.as_view(), name='signup'), 
+    url(r'accounts/login/$', LoginView.as_view(), name='login'), 
+
+    url(r'^admin/', include(admin.site.urls)),
+)
